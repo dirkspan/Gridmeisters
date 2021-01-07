059cc2eaebae41dde30c41dd520a73f2b2b4288d
anouarbousraou <70587352+anouarbousraou@users.noreply.github.com>
2021-01-07 18:24:06 +0100

    30 huizen random toegewezen aan batterij

--- code/visualisation/plot.py
+++ code/visualisation/plot.py
@@ -5,34 +5,30 @@ from matplotlib import style
 import numpy as np
 import pandas as pd
 import random
-# data inlezen
-houses = pd.read_csv('district-3_houses.csv')
-batteries = pd.read_csv('district-3_batteries.csv')
+
+houses = pd.read_csv('district-1_houses.csv')
+batteries = pd.read_csv('district-1_batteries.csv')
+
+houses1 = houses.sample(n=30)
+houses2 = houses.sample(n=30)
+houses3 = houses.sample(n=30)
+houses4 = houses.sample(n=30)
+houses5 = houses.sample(n=30)
 
 
-# figuur plotten
 fig = plt.figure()
-ax = plt.subplot(111)
-
-random.choices(char_list, k=len(real_pwd))
-# x,y = scatter
-houses = ax.scatter(houses.x, houses.y, color='k')
+ax1 = plt.subplot(111)
 
 
-batteries = ax.scatter(batteries.x, batteries.y, color='r')
-ax.set(xlabel='x-axis', ylabel='y-axis',
-      title='District 3')
+# houses = ax1.scatter(houses.x, houses.y, color='k')
+batteries = ax1.scatter(batteries.x, batteries.y, color='r')
+houses1 = ax1.scatter(houses1.x, houses1.y, color='b')
+houses2 = ax1.scatter(houses2.x, houses2.y, color='m')
+houses3 = ax1.scatter(houses3.x, houses3.y, color='k')
+houses4 = ax1.scatter(houses4.x, houses4.y, color='c')
+houses5 = ax1.scatter(houses5.x, houses5.y, color='g')
+ax1.set(xlabel='x-axis', ylabel='y-axis',
+      title='District 1')
+ax1.legend([houses, batteries], ['Houses','Batteries'], loc=3)
+fig.savefig("District1.pdf")
 
-# legenda
-ax.legend([houses, batteries], ['Houses','Batteries'], loc=3)
-
-# figuur opslaan
-fig.savefig("District3.pdf")
-
-
-# a = batteries.pop('capaciteit')
-<<<<<<< HEAD:code/visualisation/plot.py
-# b = houses.pop('maxoutput')
-=======
-# b = houses.pop('maxoutput')
->>>>>>> cdd3bf67909bf738fb9c9c1d3628b08ac7bbc1d4:docs/plot.py
