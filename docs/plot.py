cdd3bf67909bf738fb9c9c1d3628b08ac7bbc1d4
anouarbousraou <70587352+anouarbousraou@users.noreply.github.com>
2021-01-07 13:18:24 +0100

    visualisatie

--- docs/plot.py
+++ docs/plot.py
@@ -5,18 +5,26 @@ from matplotlib import style
 import numpy as np
 import pandas as pd
 
+# data inlezen
 houses = pd.read_csv('district-3_houses.csv')
 batteries = pd.read_csv('district-3_batteries.csv')
 
+# figuur plotten
 fig = plt.figure()
-ax1 = plt.subplot(111)
-houses = ax1.scatter(houses.x, houses.y, color='k')
-batteries = ax1.scatter(batteries.x, batteries.y, color='r')
-ax1.set(xlabel='x-axis', ylabel='y-axis',
+ax = plt.subplot(111)
+
+# x,y = scatter
+houses = ax.scatter(houses.x, houses.y, color='k')
+batteries = ax.scatter(batteries.x, batteries.y, color='r')
+ax.set(xlabel='x-axis', ylabel='y-axis',
       title='District 3')
-ax1.legend([houses, batteries], ['Houses','Batteries'], loc=3)
+
+# legenda
+ax.legend([houses, batteries], ['Houses','Batteries'], loc=3)
+
+# figuur opslaan
 fig.savefig("District3.pdf")
 
 
-a = batteries.pop('capaciteit')
-b = houses.pop('maxoutput')
+# a = batteries.pop('capaciteit')
+# b = houses.pop('maxoutput')
