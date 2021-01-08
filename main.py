4d9f1084b36bd258fc2a81b4bce79a98e208e816
anouarbousraou <70587352+anouarbousraou@users.noreply.github.com>
2021-01-08 15:27:41 +0100

    push

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # from code.classes import *
-# from code.classes import load_data
+from code.classes.load_data import Load_data
 # from code.classes import plot
 from code.classes.plot import plot
 # from code.visualisation import *
 
 if __name__ == "__main__":
-    return(plot())
+    load_data = Load_data()
+    houses = load_data.load_houses()
