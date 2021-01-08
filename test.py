52b96193eb1351ef665d20fa9e0617c850a54ff6
Sofie van Haasteren <Sofievanhaasteren@outlook.com>
2021-01-08 15:59:28 +0100

    updated distance calculator

--- test.py
+++ test.py
@@ -18,36 +18,45 @@ import csv
 battery_list = []
 house_list = []
 
-with open('district-1_batteries.csv', 'r') as battery_data:
+with open('data/district_1/district-1_batteries.csv', 'r') as battery_data:
     battery_reader = csv.DictReader(battery_data)
     for row in battery_reader:
         battery_list.append(row)
 
-with open('district-1_houses.csv', 'r') as houses_data:
+with open('data/district_1/district-1_houses.csv', 'r') as houses_data:
     houses_reader = csv.DictReader(houses_data)
     for row in houses_reader:
         house_list.append(row)
 
-
 total_distance = []
+
 for i in house_list:
     xh = int(i['x'])
     yh = int(i['y'])
+    id = int(i['id'])
+    print(id)
+
+    total_distance = []
 
 
     for i in battery_list:
+        id = i['id']
+        print(id)
         xb = int(i['x'])
         yb = int(i['y'])
 
         xt = abs(xh - xb)
         yt = abs(yh - yb)
-
         dis = xt + yt
+        print(dis)
+    
         total_distance.append(dis)
 
 
-minimaal = min(total_distance)
-print(minimaal)
+    minimaal = min(total_distance)
+    print(minimaal)
+
+
 
 
 
