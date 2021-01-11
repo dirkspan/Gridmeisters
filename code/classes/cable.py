832a1fbda4d9c2a166bec835496e64094c43e320
Sofie van Haasteren <Sofievanhaasteren@outlook.com>
2021-01-11 12:41:30 +0100

    updated cable and grid files

--- code/classes/cable.py
+++ code/classes/cable.py
@@ -1,53 +1,51 @@
 # test document voor het maken van de route X Sofie
 class Cable(object):
+
     def __init__(self, cable_id):
        
-        self._id = cable_id
-        self._battery_id = ""
-        self._route = []
+        self.id = cable_id
+        self.battery_id = ""
+        self.route = []
 
     def __str__(self):
         
-        return (f"Cable: {self._id}\n"
-                f"From house: {self._id} to battery {self._battery_id}\nRoute: {self._route}")
+        return (f"Cable: {self.id}\n"
+                f"From house: {self.id} to battery {self.battery_id}\nRoute: {self.route}")
 
-    def get_id(self):
+    # def get_id(self):
      
-        return self._id
+    #     return self._id
 
-    def get_batt(self):
+    # def get_batt(self):
      
-        return self._battery_id
+    #     return self._battery_id
 
-    def get_price(self):
-      
-        return self._price
 
-    def get_length(self):
+    # def get_length(self):
         
-        start = self._route[0]
-        end = self._route[-1]
-        return abs(start[0] - end[0]) + abs(start[1] - end[1])
+    #     start = self._route[0]
+    #     end = self._route[-1]
+    #     return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
-    def get_route(self):
+    # def get_route(self):
     
-        return self._route
+    #     return self._route
 
-    def add_batt(self, batt_id):
+    # def add_batt(self, batt_id):
         
-        self._battery_id = batt_id
+    #     self._battery_id = batt_id
 
-    def add_house(self, house_id):
+    # def add_house(self, house_id):
        
-        self._house_id = house_id
+    #     self._house_id = house_id
 
-    def add_route(self, house, battery):
+    # def add_route(self, house, battery):
      
-        self._route.append((house[0], house[1]))
-        self._route.append((battery[0], house[1]))
-        self._route.append((battery[0], battery[1]))
+    #     self._route.append((house[0], house[1]))
+    #     self._route.append((battery[0], house[1]))
+    #     self._route.append((battery[0], battery[1]))
 
-    def change_route(self, start, end):
+    # def change_route(self, start, end):
        
-        self._route = []
-        self.add_route(start, end)
+    #     self._route = []
+    #     self.add_route(start, end)
--- code/classes/grid.py
+++ code/classes/grid.py
@@ -3,107 +3,76 @@ class Grid(object):
 
     def __init__(self, id, x_max, y_max):
       
-        self._id = id
-        self._x_max = x_max
-        self._y_max = y_max
-        self._cables = {}
-        self._batteries = {}
-        self._houses = {}
+        self.id = id
+        self.x_max = x_max
+        self.y_max = y_max
+        self.cables = {}
+        self.batteries = {}
+        self.houses = {}
 
     def __str__(self):
         
         batteries = ""
-        for key in self._batteries:
-            if not batteries:
-                batteries += "\n" + self.get_battery(key).__str__()
-            else:
-                batteries += "\n\n" + self.get_battery(key).__str__()
-
+        for key in self.batteries:
+            batteries += "\n" + self.get_battery(key).__str__()
+           
         houses = ""
-        for key in self._houses:
-            if not houses:
-                houses += "\n" + self.get_house(key).__str__()
-            else:
-                houses += "\n\n" + self.get_house(key).__str__()
+        for key in self.houses:
+            houses += "\n" + self.get_house(key).__str__()
+            
 
         cables = ""
-        for key in self._cables:
-            if not cables:
-                cables += "\n" + self.get_cable(key).__str__()
-            else:
-                cables += "\n\n" + self.get_cable(key).__str__()
+        for key in self.cables:
+            cables += "\n" + self.get_cable(key).__str__()
 
         return (f"District: {self._id}\nx max: {self._x_max}\ny max: {self._y_max}\n\nbatteries:{batteries}\n\n"
                 f"houses:{houses} \n\ncables:{cables}")
 
 
-
-    def get_id(self):
+    # def get_id(self):
         
-        return self._id
+    #     return self._id
 
-    def get_max(self):
+    # def get_max(self):
        
-        return self._x_max, self._y_max
+    #     return self._x_max, self._y_max
 
-    def get_house(self, id):
+    # def get_house(self, id):
        
-        return self._houses[id]
+    #     return self._houses[id]
 
-    def get_houses(self):
+    # def get_houses(self):
         
-        return self._houses
+    #     return self._houses
 
-    def get_battery(self, id):
+    # def get_battery(self, id):
        
-        return self._batteries[id]
+    #     return self._batteries[id]
 
-    def get_batteries(self):
+    # def get_batteries(self):
         
-        return self._batteries
+    #     return self._batteries
 
-    def get_cable(self, cable_id):
+    # def get_cable(self, cable_id):
         
-        return self._cables[cable_id]
+    #     return self._cables[cable_id]
 
-    def get_cables(self):
+    # def get_cables(self):
         
-        return self._cables
+    #     return self._cables
 
-    def tot_len(self):
-        # Calculate total length of all cables.
-       
-        len = 0
-        for key in self._cables:
-            len += self._cables[key].get_length()
-        return len
-
-    # Mutator methods (setters)
-    def add_house(self, house):
-        
-        if house.get_id() not in self._houses:
-            self._houses[house.get_id()] = house
-        else:
-            print("Error: Key already in _houses")
-
-    def add_battery(self, battery):
-        
-        if battery.get_id() not in self._batteries:
-            self._batteries[battery.get_id()] = battery
-        else:
-            print("Error: Key already in self._batteries")
-
-    def add_cable(self, cable):
+    
+    # def add_cable(self, cable):
       
-        cable_id = cable.get_id()
-        self._cables[cable_id] = cable
+    #     cable_id = cable.get_id()
+    #     self._cables[cable_id] = cable
 
-    def rem_cable(self, key):
+    # def rem_cable(self, key):
        
-        del self._cables[key]
+    #     del self._cables[key]
 
-    def clear_cables(self):
+    # def clear_cables(self):
       
-        self._cables = {}
-        for key in self._batteries:
-            self._batteries[key].reset_cap()
+    #     self._cables = {}
+    #     for key in self._batteries:
+    #         self._batteries[key].reset_cap()
