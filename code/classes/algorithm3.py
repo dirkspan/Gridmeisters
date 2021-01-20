70940f62a0fbdf380d60aa59cadd31751d4f736c
anouarbousraou <70587352+anouarbousraou@users.noreply.github.com>
2021-01-20 12:32:25 +0100

    push

--- code/classes/algorithm3.py
+++ code/classes/algorithm3.py
@@ -7,10 +7,8 @@ import house
 import battery
 import cables 
 import random as r
-
-import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import style
+
 
 reader = load_data.Load_data()
 batteries = reader.load_batteries()
@@ -19,20 +17,13 @@ houses = reader.load_houses()
 # huizen die niet gebruikt worden
 unused_houses = []
 
-#set costs and route to 0
-# house.routetest = []
-costs = 0
-
-# shuffle huizen
-r.shuffle(houses)
-
 for house in houses:
 
     # dictionary voor distances
     dist_dict = {}
 
-    # lijst om waarde bij te houden, weet niet zo goed hoe dit met dict werkt, vandaar lijst erbij gedaaan
-    distance_list = []
+    # lijst om waarde bij te houden
+    keep_track = []
 
     for battery in batteries:
         
@@ -40,21 +31,18 @@ for house in houses:
         if battery.capacity > house.maxoutput:
 
             # reken distance uit tussen batt en huis    
-            distance = abs(house.coordinates[0] - battery.coordinates[0]) +\
-                        abs(house.coordinates[1] - battery.coordinates[1])
+            distance = abs((house.coordinates[0] - battery.coordinates[0]) + (house.coordinates[1] - battery.coordinates[1]))
 
             # voeg distance tot aan batterij toe met batterij            
             dist_dict[battery] = distance
 
-            # hou lijst bij
-            distance_list.append(distance)
+            keep_track.append(1)
 
         else:
-            # append niks zodat we deze conditie later kunnen checken
-            distance_list.append(None)
+            #  deze conditie wordt later gecheckt
+            keep_track.append(0)
 
-    # als alles None is betekent dat het huis niet gebruikt wordt (voor nu)
-    if all(i is None for i in distance_list):
+    if all(tracker == 0 for tracker in keep_track): 
         unused_houses.append(house)
 
     else:
@@ -71,68 +59,13 @@ for house in houses:
         # connect huis aan batterij en vice versa
         house.connect_to_battery = battery
         battery.connect_house(house)
+        house.calc_costs(battery)
+        battery.coordinates_cables(battery)
 
-        # costs_house = house.calc_costs(battery)
-        # costs = costs + costs_house
+        # alle matches gevonden
+        if not unused_houses:
 
-        # we hebben alle matches gevonden
-        if len(unused_houses) == 0:
-            break
+            for battery in batteries:
 
-# print uitkomst voor alle 5 batterijen
-for battery in batteries:
-
-    # print(f"This is battery {battery.id} Houses: {battery.temp_houses_to_battery}")
-    print(f"NEW BATTERY!!")
-    print(f"location: {battery.x,battery.y}")
-    print(f"capacity: 1507.0")
-    print(f"houses:")
-
-    for house in battery.houses_to_battery:
-        house.coordinates_cables(battery)
-        print(house.cables)
-            
-        # prints output for each house, each coordinate of the cable
-        print(f"house ID: {house.id}")
-        print(f"location: {house.x,house.y},")
-        print(f"output: {house.maxoutput},")
-        print(f"cables:{house.cables}")
-
-# prints total costs, last status. 
-# print('Total costs in the end:')
-# print(costs) 
-
-
-
-
-# # Loop to plot coordinates batteries
-# i = -1
-# for battery in batteries:
-#     i += 1
-#     for house in battery.houses_to_battery:
-
-#         # print(battery.houses_to_battery)
-#         # for battery in batteries:
-#         colors = ['r', 'k', 'b', 'g', 'c']
-
-#         house_x = house.x
-#         house_y = house.y
-
-#         battery_x = battery.x
-#         battery_y = battery.y
-
-#         cutting_point_x = house.x
-#         cutting_point_y = battery.y
-
-#         #plot line between house and cutting point 
-#         x = [house_x, cutting_point_x, battery_x]
-#         y = [house_y, cutting_point_y, battery_y]
-
-#         ax = plt.subplot(111)
-
-#         houses_plt = ax.scatter(house.x, house.y, color='k', marker='*')
-#         batteries_plt = ax.scatter(battery.x, battery.y, color='r', marker='^')
-
-#         plt.plot(x,y, color= colors[i])
-
-# plt.savefig("4plot.png")
+                print(len(battery.houses_to_battery))
+                # print(f"this is: {battery}: Houses: {battery.temp_houses_to_battery}") 
