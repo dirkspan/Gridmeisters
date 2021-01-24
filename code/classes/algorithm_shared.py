17f4298dd6b1841efcca4dc5defaa963a0588e5c
Dirk Span <dirkspan@MacBook-Pro-van-Dirk.local>
2021-01-24 16:32:41 +0100

    shared

--- code/classes/algorithm_shared.py
+++ code/classes/algorithm_shared.py
@@ -14,127 +14,143 @@ reader = load_data.Load_data()
 batteries = reader.load_batteries()
 houses = reader.load_houses()
 
-# shuffle houses
-random.shuffle(houses)
+def second_algorithm():
+    """
+    Schrijf hier je code voor tweede algoritme, je hoeft niks te printen
+    of returnen, zorg er alleen voor dat de huizen geappend worden aan alle batterijen
+    """
+
+    # shuffle houses
+    random.shuffle(houses)
+
+    # initialize
+    total_costs = 0
+    cables_coordinates = []
+    count_houses = 0
+
+    batterijen_lijst = []
+    for battery in batteries:
+        battery_dict ={"location": battery.coordinates, "capacity": battery.capacity, "houses":[]}
+        batterijen_lijst.append(battery_dict)
 
 
+    # for house in houses: 
 
-# empty list of coordinates
-cables_coordinates = []
+        # battery_dict["capacity"] = battery_dict["capacity"] - house.maxoutput
+        # print (battery_dict["capacity"])
 
-# initialize houses count
-count_houses = 0
-
-
-# first add the batteries as connect option and add as first items in dictionary
-for battery in batteries:
-    cables_coordinates.append(battery.coordinates)
-        
-# loop for house in houses
-for house in houses:
-  
-    # empty list for connect options 
-    connect_options = []
-
-    # keep track of distances between each house and battery
-    dist_dict = {}
-
-    # for all connect options calculate distance to house and save in list
-    for cable_point in cables_coordinates:
-        distance = int(abs(house.coordinates[0] - cable_point[0]) + abs(house.coordinates[1] - cable_point[1]))
-        connect_options.append(distance)
-
-        # add all connect options to distance dictionary
-        dist_dict[cable_point] = distance 
+    # print(batterijen_lijst)
     
-    # return closest distance of a house to all connect optionss
-    closest_distance = min(dist_dict.items(), key=lambda x: x[1])
-    closest_connection = closest_distance[0]
-    connection = closest_connection
-    
-    x_dist = abs(house.x - connection[0])
-    y_dist = abs(house.y - connection [1])
-    
-    
-    house_costs = (x_dist + y_dist) * 9
 
-    print(f"costs for this house cable: {house_costs}")
+    # for battery in batteries:
 
-    # start is house, end is connection coordinates
-    current_x = house.x
-    end_x= connection[0]
-    current_y = house.y
-    end_y = connection [1]
-    
-    # only if house isn't already connected, append first coordinate
-    if house.coordinates != connection: 
-        house.cables.append((current_x, current_y))
+    #     for house in houses:
 
-    # make the route, while the coordinates of the route aren't the coordinates of the right battery: move
-    if current_y < end_y:
-        while current_y < end_y:
-            current_y += 1
-            house.cables.append((current_x, current_y))
-    elif current_y > end_y:
-        while current_y > end_y:
-            current_y -= 1
-            house.cables.append((current_x, current_y))
-    if current_x < end_x:
-        while current_x < end_x:
-            current_x += 1
-            house.cables.append((current_x, current_y))
-    elif current_x > end_x:
-        while current_x > end_x:
-            current_x -= 1
-            house.cables.append((current_x, current_y))
-    
-     # if there is already a connection for this house
-    if house.coordinates == connection:
-        battery.connect_house(house)
-        print("verbind2")
+    #         battery_dict["capacity"] - house.maxoutput
+    #         if battery_dict["capacity"] - house.maxoutput < 0:
+    #             battery_dict["capacity"] - house.maxoutput
+    #             battery_dict["houses"].append(house.id)
 
-    # loops through dictionary and cables, to compare
-    for cable_point in house.cables:
+        # print(battery_dict["capacity"])
+
+
+    # first add the batteries as connect option and add as first items in dictionary
+    # for battery in batteries:
+    #     cables_coordinates.append(battery.coordinates)
+    #     print(cables_coordinates)
+
+    for battery in batteries:
+
+        cables_coordinates.append(batteries[4].coordinates)
+        print(cables_coordinates)
+        # loop for house in houses
+        for house in houses:
             
-        # find connection point in dictionary 
-        if cable_point == connection:
-                
-            # connect battery
-            battery.connect_house(house)
+            # empty list for connect options 
+            connect_options = []
 
-            # waar dit niet staat gaat het dus fout....
-            print("verbonden4")
+            # keep track of distances between each house and battery
+            dist_dict = {}
 
-    # if it is a new coordinate add to dictionary with right connected battery and add to cables_Coordinates
-    for tuple in house.cables:
-        if tuple not in cables_coordinates:
-            cables_coordinates.append(tuple)
+            # for all connect options calculate distance to house and save in list
+            for cable_point in cables_coordinates:
+                distance = int(abs(house.coordinates[0] - cable_point[0]) + abs(house.coordinates[1] - cable_point[1]))
+                connect_options.append(distance)
 
+                # print(cable_point)
 
+                # add all connect options to distance dictionary
+                dist_dict[cable_point] = distance 
+                # print(dist_dict[cable_point])
             
-# print output, house id, coordinates and coordinates of closest item
-    print(f"id: {house.id}")
-    print(f"house: ({house.x}, {house.y})")
-    print(f"closest_connection: {closest_connection}")
+            # return closest distance of a house to all connect optionss
+            closest_distance = min(dist_dict.items(), key=lambda x: x[1])
+            closest_connection = closest_distance[0]
+            connection = closest_connection
+            
+            x_dist = abs(house.x - connection[0])
+            y_dist = abs(house.y - connection [1])
+            
+            
+            house_costs = (x_dist + y_dist) * 9
 
+            total_costs += house_costs 
 
-# cable costs calculation, calculate costs for all grid segments, so its min start point battery
-number_of_cables = int(len(cables_coordinates))
-costs_all_cables = (number_of_cables * 9) - len(batteries)*9
-print(f"costs all cables: {costs_all_cables}")
+            # print(f"costs for this house cable: {house_costs}")
 
+            # start is house, end is connection coordinates
+            current_x = house.x
+            end_x= connection[0]
+            current_y = house.y
+            end_y = connection [1]
+            
+            # only if house isn't already connected, append first coordinate
+            if house.coordinates != connection: 
+                house.cables.append((current_x, current_y))
 
-for battery in batteries:
-    count_houses = count_houses + len(battery.houses_to_battery)
-    print(f"number of houses to {battery.id} : {len(battery.houses_to_battery)}")
-print(f"number of houses connected: {count_houses}")    
+            # make the route, while the coordinates of the route aren't the coordinates of the right battery: move
+            if current_y < end_y:
+                while current_y < end_y:
+                    current_y += 1
+                    house.cables.append((current_x, current_y))
+            elif current_y > end_y:
+                while current_y > end_y:
+                    current_y -= 1
+                    house.cables.append((current_x, current_y))
+            if current_x < end_x:
+                while current_x < end_x:
+                    current_x += 1
+                    house.cables.append((current_x, current_y))
+            elif current_x > end_x:
+                while current_x > end_x:
+                    current_x -= 1
+                    house.cables.append((current_x, current_y))
+            
+            # if there is already a connection for this house
+            if house.coordinates == connection:
+                battery.connect_house(house)
+                # print("verbind2")
 
+            # loops through dictionary and cables, to compare
+            for cable_point in house.cables:
+                    
+                # find connection point in dictionary 
+                if cable_point == connection:
+                        
+                    # connect battery
+                    battery.connect_house(house)
 
-ax = plt.subplot(111)
+                    # waar dit niet staat gaat het dus fout....
+                    # print("verbonden4")
 
-for battery in batteries:
-    batteries_plt = ax.scatter(battery.x, battery.y, color='r', marker='^')
-for house in houses:
-    houses_plt = ax.scatter(house.x, house.y, color='k', marker='*')
-    plt.plot(*zip(*house.cables), color='k')
-plt.savefig("testshared.png")
+            # if it is a new coordinate add to dictionary with right connected battery and add to cables_Coordinates
+            for tuple in house.cables:
+                if tuple not in cables_coordinates:
+                    cables_coordinates.append(tuple)
+
+            battery.capacity = battery.capacity - house.maxoutput
+            if battery.capacity - house.maxoutput < 0:
+                break
+
+    print (battery.capacity)
+    return total_costs
