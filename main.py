ac8390de38945360d74886818cd91e330e403429
Sofie van Haasteren <Sofievanhaasteren@outlook.com>
2021-01-25 15:15:17 +0100

    merged algorithms

--- main.py
+++ main.py
@@ -19,11 +19,6 @@ if len(argv) < 3:
     print("Type in the correct files, f.e: python3 main.py data/dist1_b.csv data/dist1_h.csv")
     exit()
 else:
-<<<<<<< HEAD
-    algorithms.ConstraintRelexation.optimum_creating()
-=======
->>>>>>> c6691eea274f19db8389869a4f4c3280cf561ae8
-
     print("Please select an algorithm to run: 1 = Randomize, 2 = HillClimber, 3 = Constraint relaxation")
     print("Please be aware that the first two algorithms both have overlapping cables")
 
@@ -46,10 +41,10 @@ else:
             algorithms.DistToBattery.run_output()
 
     elif algo_choice == '3':
-        algorithms.test_shared.first_algorithm()
-        plot_choice = input("Do you like to see the visualization and output?, Y/N: ")
-        if plot_choice == "Y":
-            algorithms.test_shared.constraint_relaxation()
+        algorithms.ConstraintRelexation.optimum_creating()
+        # plot_choice = input("Do you like to see the visualization and output?, Y/N: ")
+        # if plot_choice == "Y":
+        #     algorithms.test_shared.constraint_relaxation()
 
     elif algo_choice not in ['1', '2', '3']:
         print("Please make a choice between 1, 2 or 3")
