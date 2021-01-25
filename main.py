8a8d05b7dc4e353aa69e0e77f3747e4e50dc389f
anouarbousraou <70587352+anouarbousraou@users.noreply.github.com>
2021-01-25 10:51:00 +0100

    push

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ if len(argv) < 3:
     print("Type in the correct files, f.e: python3 main.py data/dist1_b.csv data/dist1_h.csv")
     exit()
 else:
-    # dit is hillclimber algoritme
-    print(f"The total costs for this district are: {algorithms.DistToBattery.first_algorithm()}\nThe figure will be saved in the directory look for figure.png")
-    algorithms.DistToBattery.plot_first_algorithm()
-    # algorithms.DistToBattery.run_output()
+    # # dit is hillclimber algoritme
+    # print(f"The total costs for this district are: {algorithms.DistToBattery.first_algorithm()}\nThe figure will be saved in the directory look for figure.png")
+    # algorithms.DistToBattery.plot_first_algorithm()
+    # # algorithms.DistToBattery.run_output()
+
+    # dit is AlgoritmeTwee, comment het uit om te runnen
+    print(f"The total costs for this district are: {algorithms.AlgoritmeTwee.second_algorithm()}\nThe figure will be saved in the directory look for figure2.png")
+    algorithms.AlgoritmeTwee.plot_second_algorithm()
+    algorithms.AlgoritmeTwee.run_sec_output
 
     # dit is random algoritme, comment het uit om te runnen
     # algorithms.Random.random_algorithm()
